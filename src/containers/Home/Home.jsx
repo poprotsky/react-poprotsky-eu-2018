fdb444a7de23040d44274749f0566b4d510a9532
Eugene Poprotsky <eugene@poprotsky.eu>
2018-06-10 10:02:32 +0200

    data.json, simple headings, homepage text sections, butgfix

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import renderHTML from 'react-render-html'
+
+import data from 'Assets/data.json'
+
+import ContactData from '../../components/ContactData/'
 
 export default class Home extends Component {
   render() {
+    const aboutShortData = renderHTML(data.aboutShortData)
     return (
       <div className='main__inner home'>
         <div className='container-fluid'>
-          <h1>Homepage</h1>
+
+        </div>
+        <div className='section dark-bg'>
+          <div className='container'>
+            <div className="heading-simple">About</div>
+            {aboutShortData}
+          </div>
+        </div>
+        <div className='section'>
+          <div className='container'>
+            <div className="heading-simple">Contact</div>
+            <ContactData data={data.contactData} />
+          </div>
         </div>
       </div>
     )
