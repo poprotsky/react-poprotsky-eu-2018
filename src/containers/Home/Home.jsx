a0cecb668ed5db71c8c1c4a42096865849f577fe
Eugene Poprotsky <eugene@poprotsky.eu>
2018-06-10 10:43:39 +0200

    grid, btn-text, fixes

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import renderHTML from 'react-render-html'
 
 import data from 'Assets/data.json'
@@ -8,6 +9,7 @@ import ContactData from '../../components/ContactData/'
 export default class Home extends Component {
   render() {
     const aboutShortData = renderHTML(data.aboutShortData)
+
     return (
       <div className='main__inner home'>
         <div className='container-fluid'>
@@ -15,13 +17,16 @@ export default class Home extends Component {
         </div>
         <div className='section dark-bg'>
           <div className='container'>
-            <div className="heading-simple">About</div>
+            <div className='heading-simple'>About</div>
             {aboutShortData}
+            <div className='btn-group'>
+              <Link to='/about' className='btn-text'>View more</Link>
+            </div>
           </div>
         </div>
         <div className='section'>
           <div className='container'>
-            <div className="heading-simple">Contact</div>
+            <div className='heading-simple'>Contact</div>
             <ContactData data={data.contactData} />
           </div>
         </div>
