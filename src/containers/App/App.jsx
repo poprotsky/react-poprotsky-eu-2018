53372f4b674f4a6100f3e9d9639f91a55c64b393
poprotsky <eugene@poprotsky.eu>
2018-09-14 15:20:56 +0200

    preloader

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -16,11 +16,11 @@ ReactGA.initialize('UA-60592089-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 export default class App extends Component {
-  // componentDidMount() {
-  //   setTimeout( () => {
-  //     document.querySelector('.preloader').classList.add('is-hidden')
-  //   }, 500 );
-  // }
+  componentDidMount() {
+    setTimeout( () => {
+      document.querySelector('.preloader').classList.add('is-hidden')
+    }, 500 );
+  }
   navOpen = () => {
     document.querySelector('.app').classList.toggle('nav-is-open')
   }
@@ -37,12 +37,6 @@ export default class App extends Component {
   render() {
     return (
       <div className='app' onClick={this.navOverlayClose}>
-        {/* <div className='preloader'>
-          <div className='preloader__inner'>
-            <h2 className='preloader__title'>Poprotsky</h2>
-            <p className='preloader__subtitle'>UI/UX/Web design &amp; UI development</p>
-          </div>
-        </div> */}
         <Favicon url={FaviconImg} />
         <Header data={data} onClickFn={this.navOpen}/>
         <main className='main'>
