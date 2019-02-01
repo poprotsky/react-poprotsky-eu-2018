19cef1e48eff49379b8a62f6bad72b7da331bf66
poprotsky <eugene@poprotsky.eu>
2019-02-01 16:09:58 +0100

    related projects

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -22,11 +22,11 @@ export default class Home extends Component {
 
     return (
       <div className='main__inner home'>
-        <PortfolioItemElected id={1} />
+        <PortfolioItemElected id={2} />
 
         <div className='container-fluid'>
           <div className='section section--homepage padding-clear--bottom'>
-            <PortfolioItemSelected data={portfolio} selected={[6,5,4,2]} />
+            <PortfolioItemSelected data={portfolio} selected={[7,6,5,3]} />
           </div>
         </div>
         <div className='section dark-bg'>
